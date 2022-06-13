0588716f431b4e2fe557f369a4ab670f2f83ee4d
Tatyana <TatyanaIvanova@gmail.com>
2022-06-13 19:20:24 +0300

    Add gb3

---
+++ gb3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8
+Console.Write("Введите числоо: ");
+int number = ConvertToInt32(Console.ReadLine());
+
+int min = number % 2;
+while (min <= number)
+{
+     Console.Write("четные числа:      ");
+    min++;
+
+}

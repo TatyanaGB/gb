5b31b053a737bbfa8f9e3521c3ef0130d5eaf280
Tatyana <TatyanaIvanova@gmail.com>
2022-06-13 20:24:08 +0300

    Add gb04

--- gb4/Program.cs
+++ gb4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4
+Console.WriteLine("Введите число a: ");
+int a = ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b:  ");
+int b = ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число c:  ");
+int c = ToInt32(Console.ReadLine());
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max=  ");

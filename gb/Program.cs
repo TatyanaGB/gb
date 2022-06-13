fc0ea0df8cb7da5a92de4d3cd519d3f05db0539f
Tatyana <TatyanaIvanova@gmail.com>
2022-06-13 19:35:33 +0300

    Add gb1

---
+++ gb/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2//
+Console.Write("Введите число a: ");
+int а = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a > b;
+if (max = a)
+ 
+{
+    Console.WriteLine("max a =  ");
+}
+
+else 
+{
+    Console.WriteLine("max b = ");
+}
+
+
